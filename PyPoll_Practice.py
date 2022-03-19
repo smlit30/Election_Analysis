a8b7bec2aa54900898e7bc33c74c2e1390158485
smlit30 <97806497+smlit30@users.noreply.github.com>
2022-03-19 10:04:07 -0500

    Add files via upload

--- PyPoll_Practice.py
+++ PyPoll_Practice.py
@@ -24,6 +24,11 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 total_votes = 0
 candidate_options = []
 candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+
 
 #open teh election results and read the file
 with open(file_to_load) as election_data:
@@ -43,7 +48,23 @@ with open(file_to_load) as election_data:
 for candidate_name in candidate_votes:
     votes = candidate_votes[candidate_name]
     vote_percentage = float(votes) / float(total_votes)*100
-    print(f"{candidate_name}: received {'%.1f'%vote_percentage}% of the vote.") 
+    #deteremine winning count & candidate
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+          
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name
+ 
+winning_candidate_summary = (
+     f"----------------------------------\n"
+     f"Winner: {winning_candidate}\n"
+     f"Winning Vote Count: {winning_count:,}\n"
+     f"Winning Percentage: {winning_percentage:.1f}%\n"
+     f"----------------------------------\n")
+print(winning_candidate_summary)
+  
+    #print(f"{candidate_name}: received {'%.1f'%vote_percentage}% of the vote.") 
 
 #print(candidate_votes)
    
